fdd7ca7277356fbf048e80455a2aed2b466d505f
Mohammed-19992 <abusalaharafat@hotmail.com>
2021-01-08 11:11:20 -0500

    updated main

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import LandingPage from './landingpage';
+import PortfolioReact from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
@@ -9,7 +9,7 @@ import Resume from './resume';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/portfolio-react" component={LandingPage} />
+    <Route exact path="/portfolio-react" component={PortfolioReact} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
